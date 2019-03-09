3ea42375c55c6227615f9d77dd770ececf19b062
Laquesisa <dana.shmaria@gmail.com>
2019-03-09 14:15:47 +0100

    Hierachical Clustering implemented and plot avaialbe

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ StartHack Microsoft Challenge, Team PancakePirates .. Mmmmmhhhh
 
 Install Python Dependencies:
 `pip install -r requirements.txt`
+
+
+Could be improved:
+ - geohash instead of only latitude and longitude
--- dna.py
+++ dna.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import pygeohash as gh
+from scipy.cluster.hierarchy import dendrogram, linkage
+import matplotlib.pyplot as plt
 
 # import dataset
 dataset = pd.read_csv("userData.csv")
@@ -11,11 +13,20 @@ dataset['gender_num'] = dataset.gender.cat.codes
 dataset.language = pd.Categorical(dataset.language)
 dataset['language_num'] = dataset.language.cat.codes
 
-dataset['geohash'] = dataset.apply(lambda x: gh.encode(x.lat, x.lng, precision=5), axis=1)
+#dataset['geohash'] = dataset.apply(lambda x: gh.encode(x.lat, x.lng, precision=5), axis=1)
+#dataset['id'] = dataset.index
 
-print(dataset)
+# plot dataset
+plotable = dataset.iloc[:, [1, 3, 4, 6, 7]]
+print(plotable)
+linked = linkage(plotable, 'single')
 
+labelList = dataset.name.values
 
-
-
-
+plt.figure(figsize=(10, 7))  
+dendrogram(linked,  
+            orientation='left',
+            labels=labelList,
+            distance_sort='descending',
+            show_leaf_counts=True)
+plt.show()
