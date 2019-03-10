b513492b1ee94ece1f41be1050f4f35291b2f4a3
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-03-10 03:25:10 +0100

    flask features

--- ml/app.py
+++ ml/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 from pymongo import MongoClient
 import pandas as pd
 from dna import import_data_frame, plot_agglomerative_cluster
+from bson.objectid import ObjectId
 client = MongoClient('mongodb://goodoldtimes:HN88VIootmZYoM1feiOq0cqpReDHOJ3wdnF5EAbD02E0qNZrLVlSTSQXuMi9XPuNPX55cbK5E4ol4m8cbYIBXg==@goodoldtimes.documents.azure.com:10255/?ssl=true&replicaSet=globaldb')
 db = client['goodoldtimes']
 
@@ -10,20 +12,21 @@ usercollection = db['users']
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "hello"
+
 @app.route('/users/<user_id>', methods = ['GET'])
 def get_recommended_user(user_id):
     if request.method == 'GET':
-        users = pd.DataFrame(list(usercollection.find()))
+        user = usercollection.find_one({"_id": ObjectId(user_id)})
+        category = user['category']
+        users = usercollection.find({"_id": {'$ne': ObjectId(user_id)}, "category": category})
 
-        dataset, additional_columns = import_data_frame(users)
-        columns = ['birthyear', 'lat', 'lng', 'gender_num', 'language_num'] + additional_columns
-        clusters= plot_agglomerative_cluster(dataset, columns)
-        dataset['category'] = clusters
-
-        print(dataset)
-
+        return jsonify(list(users))
         result = []
-        for user in usercollection.find():
+        for user in users:
+            print(user)
             result.append(user['name'])
         return ', '.join(result)
     else:
@@ -33,10 +36,28 @@ def get_recommended_user(user_id):
 @app.route('/users', methods = ['POST'])
 def create_user():
     if request.method == 'POST':
-        user = {
-            'name': 'Alex'
-        }
-        user_id = usercollection.insert_one(user).inserted_id
+        json = request.get_json()
+        # name = json["name"]
+        # age = json["age"]
+        # user = {
+        #     'name': 'Alex'
+        # }
+        # user_id = usercollection.insert_one(user).inserted_id
+        
+        users = pd.DataFrame(list(usercollection.find()))
+        # print(users)
+        dataset, additional_columns = import_data_frame(users)
+        columns = ['birthyear', 'lat', 'lng', 'gender_num', 'language_num'] + additional_columns
+        clusters= plot_agglomerative_cluster(dataset, columns)
+        dataset['category'] = clusters
+
+        for index, row in dataset.iterrows():
+            usercollection.update_one(
+                {"_id": row['_id']},
+                {"$set":
+                {"category": row['category']
+            }})
+
         #modify/update the information for <user_id>
         data = request.form
         # return user_id
